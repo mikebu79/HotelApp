a4b987367d9b13fd4c5e5325a556bf158638c9a7
Miguel Mendez <miguel.mendez@gmail.com>
2015-11-14 11:33:33 -0600

    Historial

--- js/reservas.js
+++ js/reservas.js
@@ -18,7 +18,20 @@ var almacen = {
         tx.executeSql(‘CREATE TABLE IF NOT EXISTS reservas (th, np, nh, nd)’);
         tx.executeSql(‘INSERT INTO reservas (th, np, nh, nd) VALUES(“’+almacen.th+’”, “’+almacen.np+’”, “’+almacen.nh+’”, “’+almacen.nd+’”)’);
     },
-    
+    tablaHistorial:function (tx){
+        tx.executeSql("CREATE TABLE IF NOT EXISTS historial (th, np, nh, nd)");
+        tx.executeSql("INSERT INTO historial (th, np, nh, nd) VALUES ('"+almacen.th+"','"+almacen.np+"','"+almacen.nh+"','"+almacen.nd+"')");
+    },
+     guardarHistorial:function(th,np,nh,nd){
+        //almacen.db = window.openDatabase("hotelApp", "1.0", "Hotel App", 200000);
+        almacen.th = th;
+        almacen.np = np;
+        almacen.nh = nh;
+        almacen.nd = nd;
+        
+        almacen.db.transaction(almacen.tablaHistorial, almacen.error, null);
+        
+    },    
     error: function (e) {
         alert("Error, código: "+ e.code);
     },
@@ -35,6 +48,10 @@ var almacen = {
 		for (i=0; i<response.rows.length;i++){
 			server.envRes(response.rows.item(i).th, response.rows.item(i).np, response.rows.item(i).nh, response.rows.item(i).nd);
 		}
+                tx2.executeSql("DELETE FROM reservas");
 	}, almacen.error);
-    }
+    },
+	reservaLeida: function(){
+		alert("Reservas Sincronizadas");
+	}
 }
